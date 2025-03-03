faa11f7ac8c04bb6158f7763668e152843069857
taeyoung0524 <miseoseo1@naver.com>
2025-03-03 17:34:36 +0900

    변수 예제로드

---
+++ Chap02/src/Example01.java
@@ -0,0 +1,16 @@
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int SPEED=20;
+		int count=10;
+		//SPEED=40;
+		count=11;
+		System.out.print("상수 SPEED 값은 ");
+		System.out.println(SPEED);
+		System.out.print("변수 count의 값은 ");
+		System.out.println(count);
+	}
+
+}
---
+++ Chap02/src/Variable02.java
@@ -0,0 +1,11 @@
+
+public class Variable02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		System.out.println("반지름이 3인 원의 넓이를 구합시다.");
+		System.out.print(3*3*PI);
+	}	
+
+}
