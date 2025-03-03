65403db242591e727d31e93a0e56bdcbff9a4b3b
taeyoung0524 <miseoseo1@naver.com>
2025-03-03 18:29:35 +0900

    데이터 출력 예제 추가

---
+++ Chap02/src/InputOutput01.java
@@ -0,0 +1,13 @@
+
+public class InputOutput01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "홍길동";
+		int myAge = 20;
+		
+		System.out.println("이름 : "+myName);
+		System.out.println("나이 : "+myAge);
+	}
+
+}
---
+++ Chap02/src/InputOutput02.java
@@ -0,0 +1,14 @@
+
+public class InputOutput02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String myName ="홍길동";
+		int myAge = 20;
+		
+		System.out.print("이름 : \t" + myName + "\n");
+		System.out.print("나이 : \t" + myAge + "\n");
+	}
+
+}
